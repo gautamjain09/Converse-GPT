0d0d77cc91760b719d3d01f3e02a342927e86241
gautamjain09 <gautamjain0918@gmail.com>
2023-01-25 22:06:33 +0530

    Initial Setup

---
+++ android/app/src/main/kotlin/com/gautamjain09/converse_gpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gautamjain09.converse_gpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Converse GPT',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      onGenerateRoute: onGenerateRoute.route,
+      initialRoute: '/',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
